b5f4d100ee3805d6b388a96caf0ccae3d3c7c2b4
Thomas Graber <thomas.graber310@gmail.com>
2025-05-01 16:19:30 +0200

    feat(database): seed models if needed

--- backend_persistence/app/main.py
+++ backend_persistence/app/main.py
@@ -1,11 +1,19 @@
 import os.path
+from contextlib import asynccontextmanager
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from app.api.routes import router
 from app.models import SQLModel
 from app.database import engine
+from app.seed import seed_models_if_needed
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await seed_models_if_needed()
+
+    yield  # App continue ici
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router)
---
+++ backend_persistence/app/seed.py
@@ -0,0 +1,43 @@
+import os
+
+from sqlmodel import Session, select
+
+from app.database import engine
+from app.models import ModelInfo  # adapte selon ta classe réelle
+import asyncio
+import requests
+
+async def seed_models_if_needed():
+    url = os.path.join(os.getenv("INSSURANCE_BACKEND_URL"), "models")
+
+    for attempt in range(5):
+        try:
+            response = requests.get(url, timeout=3)
+            response.raise_for_status()
+            models_data = response.json()
+            break
+        except Exception as e:
+            print(f"[Seed attempt {attempt + 1}] Backend not ready: {e}")
+            await asyncio.sleep(2)
+    else:
+        print("Backend unreachable. Skipping seeding.")
+        return
+
+    with Session(engine) as session:
+        for model_name, content in models_data.items():
+            # Vérifie s'il existe déjà
+            exists = session.exec(
+                select(ModelInfo).where(ModelInfo.name == model_name)
+            ).first()
+
+            if not exists:
+                print(f"Inserting model: {model_name}")
+                model = ModelInfo(
+                    name=model_name,
+                    metrics=content.get("metrics"),
+                    columns=content.get("columns")
+                )
+                session.add(model)
+
+        session.commit()
+        print("Seeding complete.")
